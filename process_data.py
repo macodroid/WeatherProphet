d2515fc73c8d4e8e199dcead21e8e1b36ecb516f
macodroid <macko.andrej1@gmail.com>
2023-01-24 12:52:15 +0100

    modified dataset and process_data.py. Create new column as label. separated train, val, test labels

--- process_data.py
+++ process_data.py
@@ -11,6 +11,9 @@ if __name__ == "__main__":
     weather_data = pd.read_csv(weather_data_path, parse_dates=["Date"])
     # reverse order of rows
     weather_data = weather_data.iloc[::-1]
+    weather_data.reset_index(drop=True, inplace=True)
+    # create label column
+    weather_data["next_T"] = weather_data["T"].shift(1)
 
     # split dataset into train, validation and test.
     # The train dataset will start from 2012-10-01 (yyyy-mm-dd) and end at 2020-12-31.
@@ -24,21 +27,30 @@ if __name__ == "__main__":
         train_date_end, val_date_end, weather_data
     )
 
-    train_data = helper.process_data(train_data)
-    val_data = helper.process_data(val_data)
-    test_data = helper.process_data(test_data)
+    train_features, train_labels = helper.process_data(train_data)
+    val_features, val_labels = helper.process_data(val_data)
+    test_features, test_labels = helper.process_data(test_data)
     # normalize data
     # Just for future. Inverse normalization X_unscaled = (X_scaled * std) + mean
-    train_data, mean, std = helper.normalize_data(train_data)
-    val_data = helper.normalize_data(val_data, mean, std)
-    test_data = helper.normalize_data(test_data, mean, std)
+    norm_train_features, mean, std = helper.normalize_data(train_features)
+    norm_val_features = helper.normalize_data(val_features, mean, std)
+    norm_test_features = helper.normalize_data(test_features, mean, std)
     # convert to numpy array
-    train_data, val_data, test_data = (
-        train_data.to_numpy(),
-        val_data.to_numpy(),
-        test_data.to_numpy(),
+    norm_train_features, norm_val_features, norm_test_features = (
+        norm_train_features.to_numpy(),
+        norm_val_features.to_numpy(),
+        norm_test_features.to_numpy(),
     )
-    np.save("dataset/train_data.npy", train_data)
-    np.save("dataset/val_data.npy", val_data)
-    np.save("dataset/test_data.npy", test_data)
+    train_labels, val_labels, test_labels = (train_labels.to_numpy(), val_labels.to_numpy(), test_labels.to_numpy())
+
+    # train
+    np.save("dataset/train_features.npy", norm_train_features)
+    np.save("dataset/train_labels.npy", train_labels)
+    # validation
+    np.save("dataset/val_features.npy", norm_val_features)
+    np.save("dataset/val_labels.npy", val_labels)
+    # test
+    np.save("dataset/test_features.npy", norm_test_features)
+    np.save("dataset/train_labels.npy", test_labels)
+    # normalization data
     np.save("dataset/stat.npy", np.array([mean.to_numpy(), std.to_numpy()]))
